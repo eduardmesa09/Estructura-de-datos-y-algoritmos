4f40ec5cef00ea4453e6a3693144b44b4b566919
Eduard Meza <115745218+eduardmesa09@users.noreply.github.com>
2024-07-30 19:14:43 -0500

    Updated random_quick_sort.py

--- random_quick_sort.py
+++ random_quick_sort.py
@@ -8,26 +8,42 @@ lista_5 = [6, 2, 8, 5, 4, 9, 1, 6, 8, 2]
 lista_6 = [7, 5, 3, 2, 9, 4, 8, 5, 6, 3]
 lista_7 = [2, 7, 4, 6, 9, 1, 1, 7, 3, 8]
 
+#Intercambia la posición de dos números dados en una lista.
 def swap(index_1, index_2, lista):
     lista[index_1], lista[index_2] = lista[index_2], lista[index_1]
 
-def evaluate(pivote, store_index, lista):
+#Dada la posición de un numero inicial (pivote), la posción de un número a comparar (store_index),
+#y una lista, envía al pivote a la primera posicíon de las lista usando el método swap, 
+#evalua mediante un ciclo for si el pivote es mayor al al isguiente indice en la lista, 
+#en caso tal mediante la función swap intercambia dicho indice con la posicíon del store_index
+#e incrementa el store_index +1. Al finalizar este ciclo, envía al pivote a la posición del 
+#penultimo valor del store_index.
+def ordenar(pivote, store_index, lista):
     swap(0, pivote, lista)
+    
     for i in range (1, len(lista)):
         if (lista[i] <= lista[0]):
             swap(i, store_index, lista)
             store_index += 1
+            
     swap(0, (store_index-1), lista)
-    return store_index
 
+#Dada una lista, crea una copia de dicha lista y la organiza usando el método .sort, mediante
+#un ciclo while compara si la lista inicial es igual a la lista previamente ordenada, sí no,
+# invoca la función ordenar, proporcionando una posición inicial a comparar (1), un pivote al azar
+#y una lista. Así hasta que la comparación inicial sea verdadera.
 def Random_quick_sort(lista): 
     sorted_list = lista[:]
     sorted_list.sort()
+    ciclo= 1
     
     while (lista != sorted_list) :
         pivote = random.randint(0,(len(lista)-1))
-        store_index = 1
-        evaluate(pivote, store_index, lista)
+        ordenar(pivote, 1, lista)
+        print("Ciclo : ", ciclo)
+        ciclo+= 1
+        print("Resultado :") 
         print(lista)
+        print("")
     
-Random_quick_sort(lista_1)
+Random_quick_sort(lista_2)
